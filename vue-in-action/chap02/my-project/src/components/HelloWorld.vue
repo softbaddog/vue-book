b4f81b1cfcd4ceffe598ce66ca6473edeba2c539
Fu Wei <softbaddog@gmail.com>
2018-11-27 16:00:13 +0800

    and more

---
+++ vue-in-action/chap02/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>{{count}}</span>
+    <button v-on:click="inc">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    inc() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+span {
+  color:red
+}
+</style>
